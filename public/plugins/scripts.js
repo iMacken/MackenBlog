9d3ddcdf4d8176fb1b97ade3729c5aa19f6a0dc5
Macken <rystlee100@gmail.com>
2016-10-27 15:36:37 +0800

    pjax completed

--- public/plugins/scripts.js
+++ public/plugins/scripts.js
@@ -1,38 +1,63 @@
+(function($){
 
-    //PJAX
-    // does current browser support PJAX
-    if ($.support.pjax) {
-        $.pjax.defaults.timeout = 3000; // time in milliseconds
+    var MackenBlog = {
+
+        //use pjax to load blog
+        init: function() {
+            var self = this;
+
+            $(document).pjax('a:not(a[target="_blank"])', 'body', {
+                timeout: 1600,
+                maxCacheLength: 500
+            });
+            $(document).on('pjax:start', function() {
+                NProgress.start();
+            });
+            $(document).on('pjax:end', function() {
+                NProgress.done();
+                self.blogBootUp();
+            });
+
+            self.blogBootUp();
+        },
+
+        /*
+        * Things to be execute when normal page load
+        * and pjax page load.
+        */
+        blogBootUp: function() {
+            //geopattern
+            $('.geopattern').each(function(){
+                $(this).geopattern($(this).data('pattern-id'));
+            });
+
+            //search
+            $('.navbar-form').submit(function (event) {
+                event.preventDefault();
+                var keyword = $('#search-keyword').val();
+                if ($.trim(keyword) == '') {
+                    return false;
+                }
+
+                var host = $('.navbar-form').attr('action');
+                window.location.href = host + '/' + keyword;
+            });
+
+            //share bar
+            $('.share-bar').share();
+
+            //back to top
+            $('#to-top').click(function(){
+                $('html, body').animate({scrollTop:0}, 'slow');
+            });
+        }
     }
-    $(document).pjax('a', 'body');
-    $(document).on('pjax:start', function() {
-        NProgress.start();
-    });
-    $(document).on('pjax:end', function() {
-        NProgress.done();
 
-        //geopattern
-        $('.geopattern').each(function(){
-            $(this).geopattern($(this).data('pattern-id'));
-        });
+    window.MackenBlog = MackenBlog;
 
-        //search
-        $('.navbar-form').submit(function (event) {
-            event.preventDefault();
-            var keyword = $('#search-keyword').val();
-            if ($.trim(keyword) == '') {
-                return false;
-            }
+})(jQuery);
 
-            var host = $('.navbar-form').attr('action');
-            window.location.href = host + '/' + keyword;
-        });
-
-        //share bar
-        $('.share-bar').share();
-
-        //back to top
-        $('#to-top').click(function(){
-            $('html, body').animate({scrollTop:0}, 'slow');
-        });
-    });
+$(document).ready(function()
+{
+    MackenBlog.init();
+});
